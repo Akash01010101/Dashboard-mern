dd6561857a1f257868050a9ac2fc47626a0d2061
akash jha <akashjhasciencea@gmail.com>
2025-01-17 19:11:28 +0530

    formatting

--- src/components/AdminForm.jsx
+++ src/components/AdminForm.jsx
@@ -1,35 +1,34 @@
-// src/components/AdminLogin.jsx
-
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const AdminForm = () => {
-    const [authToken, setAuthToken] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
- const nav = useNavigate();
- const handlenav = (path) => {
+  const [authToken, setAuthToken] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const nav = useNavigate();
+  const handlenav = (path) => {
     nav(path);
- }
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
-      const response = await axios.post('https://3w-backend-production.up.railway.app/api/login', {
-        username,
-        password,
-      });
+      const response = await axios.post(
+        "https://3w-backend-production.up.railway.app/api/login",
+        {
+          username,
+          password,
+        }
+      );
 
       const { token } = response.data;
       setAuthToken(token);
-      localStorage.setItem('authToken', token);
-      alert('Login successful');
+      localStorage.setItem("authToken", token);
+      alert("Login successful");
       handlenav("/admin");
     } catch (error) {
-      setError('Invalid credentials');
-      console.error('Login failed:', error);
+      setError("Invalid credentials");
+      console.error("Login failed:", error);
     }
   };
 
@@ -39,7 +38,9 @@ const AdminForm = () => {
       {error && <p className="text-red-500">{error}</p>}
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
-          <label className="block text-lg font-semibold text-gray-700">Username</label>
+          <label className="block text-lg font-semibold text-gray-700">
+            Username
+          </label>
           <input
             type="text"
             value={username}
@@ -49,7 +50,9 @@ const AdminForm = () => {
           />
         </div>
         <div>
-          <label className="block text-lg font-semibold text-gray-700">Password</label>
+          <label className="block text-lg font-semibold text-gray-700">
+            Password
+          </label>
           <input
             type="password"
             value={password}
@@ -65,8 +68,20 @@ const AdminForm = () => {
           Login
         </button>
       </form>
-      <p>Test Credentials: <div>username:'testadmin'</div> <div> password:'testadmin'</div></p>
-      <button className='w-full py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500' onClick={()=>{handlenav("/")}}>Create a User</button>
+   
+        <div>
+        Test Credentials: <div>username:'testadmin'</div>{" "}
+        <div> password:'testadmin'</div>
+        </div>
+      
+      <button
+        className="w-full py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        onClick={() => {
+          handlenav("/");
+        }}
+      >
+        Create a User
+      </button>
     </div>
   );
 };
